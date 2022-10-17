6b8f36e0493b8865c965abd9f2191702f6a2f1d3
JuanArroyo06 <juanjose753357@hotmail.com>
2022-10-16 20:59:16 -0500

    rev21.0

--- src/main/java/com/misiontic/USA/Ortesis/models/Client.java
+++ src/main/java/com/misiontic/USA/Ortesis/models/Client.java
@@ -15,10 +15,12 @@ public class Client implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idClient;
-    private String name;
+
     private String email;
-    private Integer age;
     private String password;
+    private String name;
+    private Integer age;
+
 
 
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "client")
@@ -31,20 +33,13 @@ public class Client implements Serializable {
 
     //Getter And Setter
 
+
     public Integer getIdClient() {
         return idClient;
     }
 
     public void setIdClient(Integer idClient) {
         this.idClient = idClient;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
@@ -55,20 +50,28 @@ public class Client implements Serializable {
         this.email = email;
     }
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public List<Message> getMessage() {
